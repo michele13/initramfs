e88b5dffa7d66a56b5bab4b68f1befd09a3faa50
michele13 <michele.bucca@gmail.com>
2026-03-12 11:42:00 +0100

    update_linux-live.sh asks if you want to overwrite livekit config if a new one is available

--- update_linux-live.sh
+++ update_linux-live.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 touch initramfs/bin/busybox
-cp -uv linux-live/config linux-live/livekitlib initramfs/lib/
+cp -iuv linux-live/config initramfs/lib/
+cp -uv linux-live/livekitlib initramfs/lib/
 cp -uv linux-live/initramfs/init initramfs/lib/livekit-init
 cp -uiv linux-live/initramfs/static/* initramfs/bin/
-cp -uiv linux-live/initramfs/shutdown initramfs/
+cp -uv linux-live/initramfs/shutdown initramfs/
 chmod -v a+x  initramfs/lib/livekit-init initramfs/shutdown initramfs/bin/*
 rm -v initramfs/bin/README
