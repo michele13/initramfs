cc3a079fff359da81f66cad29e8200638d9efcc3
michele13 <michele.bucca@gmail.com>
2026-03-12 11:23:53 +0100

    slax init is now livekit

--- update_linux-live.sh
+++ update_linux-live.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 touch initramfs/bin/busybox
 cp -uv linux-live/config linux-live/livekitlib initramfs/lib/
-cp -uv linux-live/initramfs/init initramfs/slax
+cp -uv linux-live/initramfs/init initramfs/lib/livekit-init
 cp -uiv linux-live/initramfs/static/* initramfs/bin/
 cp -uiv linux-live/initramfs/shutdown initramfs/
-chmod -v a+x  initramfs/slax initramfs/shutdown initramfs/bin/*
+chmod -v a+x  initramfs/lib/livekit-init initramfs/shutdown initramfs/bin/*
 rm -v initramfs/bin/README
